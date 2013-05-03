03c2582c05fce04eb8f6aff95ca5114ec347b3e5
Chris Burbridge <christopherburbridge@gmail.com>
2013-05-02 22:51:12 -0600

    Great, added rounding, opacity, and highlight — though fadeTo is not right ..

--- js/display.js
+++ js/display.js
@@ -2,10 +2,9 @@
 	"use strict";
 	$(function () {
 		
-		var myBlock = new Array();
-		var myCounter = 0;
+		$('html').append('<div id="cbbi-console"><p><strong>Elements with Background Set</strong></p><table></table></div>');
 		
-		$('html').append('<div id="cbbi-console"><p><strong>Elements with Background Set</strong></p></div>');
+		$('#cbbi-console table').append( '<thead><tr><th>Color / Transp.</th><th>Opacity</th><th>Round Corners</th><th>Highlight</th><th>&nbsp;</th></tr></thead><tbody></tbody>' );
 		
 		$('html, body, div').each(function() {
 			
@@ -21,16 +20,20 @@
 				// Ignore my own code
 				if (this_id != '#cbbi-console') {
 					
-					myCounter++;
-					myBlock[myCounter] = this_element + this_id;
-
-					var msg = '<p><input id="block-" type="checkbox" checked="checked" data-selector="'
-						+ this_element + this_id
-						+ '" data-background-css="'
+					var html = '<tr data-selector="' + this_element + this_id + '">'
+						+ '<td><input class="bg-changer" id="block-" type="checkbox" checked="checked"' 
+						+ ' data-background-css="'
 						+ $(this).css('background-color')
-						+ '"/> <strong>' 
-						+ this_element + this_id + '</strong> - ' + $(this).css('background-color') + '</p>';
-					$('#cbbi-console').append(msg);
+						+ '"/></td>'
+						+ '<td><input class="alphaChannel" type="checkbox" /></td>'
+						+ '<td><input class="rounded" type="checkbox" /></td>'
+						+ '<td><input class="highlight" type="checkbox" /></td>'
+						+ '<td><strong>' 
+						+ this_element + this_id + '</strong>'
+						// + '<br/>' + $(this).css('background-color') 
+						+ '</td>';
+
+					$('#cbbi-console tbody').append(html);
 
 				}
 				
@@ -38,19 +41,52 @@
 			
 		})
 		
+		
 		$('#cbbi-console :checkbox').click(function() {
 		    var $this = $(this);
-				var selector;
-				var backgroundCSS;
-		    // $this will contain a reference to the checkbox   
-		    if ($this.is(':checked')) {
-					selector = $(this).attr('data-selector');
-					backgroundCSS = $(this).attr('data-background-css');
-		    } else {
-					selector = $(this).attr('data-selector');
-					backgroundCSS = 'transparent';
-		    }
-				$(selector).css('background-color', backgroundCSS);
+				var selector = $(this).parents('tr').attr('data-selector');
+				
+				if ( $(this).hasClass('bg-changer') ) {
+					var backgroundCSS;
+			    if ($this.is(':checked')) {
+						backgroundCSS = $(this).attr('data-background-css');
+			    } else {
+						backgroundCSS = 'transparent';
+			    }
+					$(selector).css('background-color', backgroundCSS);
+				}
+				
+				else if ( $(this).hasClass('rounded') ) {
+					var rounding;
+			    if ($this.is(':checked')) {
+						rounding = '17px';
+			    } else {
+						rounding = '0';
+			    }
+					$(selector).css('border-radius', rounding);
+					$(selector).css('-webkit-border-radius', rounding);
+					$(selector).css('-moz-border-radius', rounding);
+				}
+
+				else if ( $(this).hasClass('highlight') ) {
+					var outline;
+			    if ($this.is(':checked')) {
+						outline = '11px solid rgba(42, 67, 255, 0.3)';
+			    } else {
+						outline = 'none';
+			    }
+					$(selector).css('outline', outline);
+				}
+
+				else if ( $(this).hasClass('alphaChannel') ) {
+					var fade;
+			    if ($this.is(':checked')) {
+						fade = '.5';
+			    } else {
+						fade = '1';
+			    }
+					$(selector).fadeTo( .3, fade )
+				}
 		});
 		
 		
